8c328e1036b9272c6bf9392b775b2e400bef9881
Tom <tomrambo95@yahoo.com>
2020-09-23 20:39:29 -0400

    added ProgramIU class/cleaned up Program class/ WIP

--- Drofsnar the Bird-Man/Birds.cs
+++ Drofsnar the Bird-Man/Birds.cs
@@ -143,7 +143,146 @@ namespace Drofsnar_the_Bird_Man
         //{
         //    Score += 200;
         //}
-        
+
+        public string GetText()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\ElevenFiftyProjects\game-sequence.txt");
+            string wordCount = text;
+            return wordCount;
+        }
+        public List<Birds> countList = new List<Birds>();
+
+        public int CountBird()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getBird().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            //int count = birdCount * 10;
+            return birdCount;
+        }
+
+        public int CountCrestedIbis()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getCrestedIbis().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount * 100;
+            return count;
+        }
+
+        public int CountGreatKiskudee()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getGreatKiskudee().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount * 300;
+            return count;
+        }
+
+        public int CountRed_NeckedPhalarope()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getRed_NeckedPhalarope().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount * 700;
+            return count;
+        }
+
+        public int CountRedCrossbill()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getRedCrossbill().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount * 500;
+            return count;
+        }
+
+        public int CountEveningGrosbeak()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getEveningGrosbeak().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount * 1000;
+            return count;
+        }
+
+        public int CountGreaterPrairieChicken()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getGreaterPrairieChicken().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount * 2000;
+            return count;
+        }
+
+        public int CountIcelandGull()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getIcelandGull().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount * 3000;
+            return count;
+        }
+
+        public int CountOrange_BelliedParrot()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getOrange_BelliedParrot().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount * 5000;
+            return count;
+        }
+
+        public int CountVulnerableBirdHunter()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getVulnerableBirdHunter().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int count = birdCount + 200 + 400 + 800 + 1600;
+            return count;
+        }
+
+        public int CountInvincibleBirdHunter()
+        {
+            Birds birds = new Birds();
+            string[] source = GetText().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == birds.getInvincibleBirdHunter().ToLowerInvariant()
+                             select word;
+            int birdCount = matchQuery.Count();
+            int lifeCount = birdCount - 5;
+            return lifeCount;
+        }
         public void PrintScoreBoard()
         {
             Console.WriteLine($"Score: {Score}| Lives: {Lives}" );
---
+++ Drofsnar the Bird-Man/ProgramIU.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drofsnar_the_Bird_Man
+{
+    public class ProgramIU : Birds
+    {
+        public void Run()
+        {
+            Birds birds = new Birds();
+
+            Console.WriteLine($"Bird score count:  {birds.CountBird()}");
+            Console.WriteLine($"Crested Ibis score count:  {birds.CountCrestedIbis()}");
+            Console.WriteLine($"Great Kiskudee score count:  {birds.CountGreatKiskudee()}");
+            Console.WriteLine($"Red-Necked Phalarope score count:  {birds.CountRed_NeckedPhalarope()}");
+            Console.WriteLine($"Red Crossbill score count:  {birds.CountRedCrossbill()}");
+            Console.WriteLine($"Evening Grosbeak score count:  {birds.CountEveningGrosbeak()}");
+            Console.WriteLine($"Greater Prairie Chicken score count:  {birds.CountGreaterPrairieChicken()}");
+            Console.WriteLine($"Iceland Gull score count:  {birds.CountIcelandGull()}");
+            Console.WriteLine($"Orange-Bellied Parrot score count:  {birds.CountOrange_BelliedParrot()}");
+            Console.WriteLine($"Vulnerable Bird Hunter count:  {birds.CountVulnerableBirdHunter()}");
+            Console.WriteLine($"Invincible Bird Hunter count:  {birds.CountInvincibleBirdHunter()}");
+            //birds.Score = CountBird() + CountCrestedIbis() + CountGreatKiskudee() + CountRedCrossbill() + CountRed_NeckedPhalarope() + CountEveningGrosbeak() + CountGreaterPrairieChicken() + CountIcelandGull() + CountOrange_BelliedParrot();
+            //birds.Lives = CountInvincibleBirdHunter();
+            //Console.WriteLine("\n");
+            birds.PrintScoreBoard();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
